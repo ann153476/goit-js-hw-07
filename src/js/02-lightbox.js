4b9d40d208a31bc4b0322ff58566416a7b2e8be2
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-11 14:22:22 +0200

    new

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,11 +2,13 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(".gallery");
 
+
 const markup = galleryItems
 .map((galleryItem) =>
  `<a class="gallery__item" href="${galleryItem.original}">
  <img class="gallery__image" 
  src="${galleryItem.preview}" 
+ data-source="${galleryItem.description}"
  alt="${galleryItem.description}" />
 </a>`).join("");
 
@@ -17,13 +19,28 @@ const markup = galleryItems
     if(event.target.nodeName !== "IMG"){
         return;
     };
-    let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-	// do something…
-});
-}
+    let gallery = new SimpleLightbox('.gallery a',
+    {captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",}
+    );
+    gallery.on('show.simplelightbox', function () {
+      
+    })
+  //     console.log(event.target);
+  //     console.log(event.target.dataset.source);
+
+  //  const markup = `<div class="signature">
+  //  ${event.target.dataset.source}</div>`;
+
+  //    event.target.insertAdjacentElement("afterend", markup);   
+  }
+
+      
 
 
   gallery.addEventListener('click', imgClick );
 
-console.log(galleryItems);
+
+	
+
